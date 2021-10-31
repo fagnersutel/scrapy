f45e1592f648494d4ca3ed53a1b25b15f93dd41d
Fagner Sutel de Moura <fagnersutel@hotmail.com>
2021-10-31 09:28:43 -0300

    Delete cousera/cousera/spiders directory

--- cousera/cousera/spiders/courseracurses.py
+++
@@ -1,9 +0,0 @@
-import scrapy
-
-
-class CourseracursesSpider(scrapy.Spider):
-    name = 'courseracurses'
-    start_urls = ['https://www.coursera.org/browse?languages=pt']
-
-    def parse(self, response):
-        self.log('Hello World! Scrapy Project')
